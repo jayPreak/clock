3af86b7770a44d24590451be9e39356b9684c766
jayPreak <jayesh.preak2003@gmail.com>
2022-12-05 13:28:10 +0530

    js 🐇🐇

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 console.log("yep")
+let hour = document.querySelector('.hour')
+let minute = document.querySelector('.minute')
+let second = document.querySelector('.second')
 
 setInterval(() => {
     const now = new Date()
-    
+    hourDig = now.getHours()
+    minDig = now.getMinutes()
+    secDig = now.getSeconds()
+
+    hour.innerHTML = hourDig
+    minute.innerHTML = minDig
+    second.innerHTML = secDig
+
 }, 1000)
