d418e094f77e88dc02223cb02d5d00f489c63d4e
jayPreak <jayesh.preak2003@gmail.com>
2022-12-05 13:34:05 +0530

    okk done

--- script.js
+++ script.js
@@ -9,6 +9,14 @@ setInterval(() => {
     minDig = now.getMinutes()
     secDig = now.getSeconds()
 
+    if(hourDig < 10) {
+        hourDig = '0' + hourDig
+    } else if (minDig < 10) {
+        minDig = '0' + minDig
+    } else if (secDig < 10) {
+        secDig = '0' + secDig
+    }
+
     hour.innerHTML = hourDig
     minute.innerHTML = minDig
     second.innerHTML = secDig
